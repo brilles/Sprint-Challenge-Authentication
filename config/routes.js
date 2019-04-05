73f6e287cea6921118d4c90490f50cd9a0d2224f
Brian Illes <brianrilles@gmail.com>
2019-04-05 08:29:55 -0700

    Implements register function

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
+const Users = require('../users/users-model.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,8 +10,18 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  let user = req.body;
+  const hashedPassword = bcrypt.hashSync(user.password, 8);
+  user.password = hashedPassword;
+
+  try {
+    const addedUser = await Users.add(user);
+    res.status(201).json(addedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'User could not be added.' });
+  }
 }
 
 function login(req, res) {
@@ -18,7 +30,7 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: 'application/json' }
   };
 
   axios
