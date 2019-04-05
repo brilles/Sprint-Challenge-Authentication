289506d6337fd2a1f778919252f81e3bd6998b0b
Brian Illes <brianrilles@gmail.com>
2019-04-05 09:23:19 -0700

    Routes created

--- client/jokes/src/App.js
+++ client/jokes/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import './App.css';
+import Signup from './auth/Signup.js';
+import Signin from './auth/Signin.js';
+import Jokes from './jokes/Jokes.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>SUCCESS FROM APP</p>
+        <header>
+          <NavLink to="/signup">Sign Up</NavLink>
+          <NavLink to="/signin">Sign In</NavLink>
+          <NavLink to="/jokes">Jokes</NavLink>
+          <button>Log out</button>
+        </header>
+        <div className="main">
+          <Route path="/signup" component={Signup} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/jokes" component={Jokes} />
+        </div>
       </div>
     );
   }
---
+++ client/jokes/src/jokes/Jokes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Jokes() {
+  return (
+    <div>
+      <p>hello from jokes</p>
+    </div>
+  );
+}
